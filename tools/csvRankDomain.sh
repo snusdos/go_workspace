75f28d07bd5eb46ef873526588e92ba42bf3e4de
Simon Stensson <simon_stensson@hotmail.com>
2024-05-02 00:14:16 +0200

    edited csrankdomain fewer fields

--- tools/csvRankDomain.sh
+++ tools/csvRankDomain.sh
@@ -2,23 +2,20 @@
 # columns. Also fills in the headers.
 # For performance reasons, this might be worth to do in 
 # pemToTxt.sh and also using parallel.
+#time bash ./tools/csvRankDomain.sh         
 echo "Creating result.csv"
-printf "rank\tdomain\tserialnumber\tsubjectC\tsubjectCN\tsubjectL\tsubjectO\tsubjectOU\tissuerC\tissuerCN\tissuerL\tissuerO\tissuerOU\ttypeOfCert\tSCTs\tsignatureAlgorithm\tpublicKeyAlgorithm\tCRL\tOCSP\tvalidity\tkeyLength\tpolicyOID\tCPS\n" > result.csv
+printf "fname\tserialnumber\tsubjectC\tsubjectCN\tsubjectL\tsubjectO\tsubjectOU\tissuerC\tissuerCN\tissuerL\tissuerO\tissuerOU\ttypeOfCert\tSCTs\tsignatureAlgorithm\tpublicKeyAlgorithm\tCRL\tOCSP\tvalidity\tkeyLength\n" > result.csv
 
 # Compile the C script
-gcc -std=c99 scripts/c_scripts/getBasics.c -O3 -o scripts/c_scripts/getBasics
+gcc -std=c99 tools/c_scripts/getBasics.c -O3 -o tools/c_scripts/getBasics
 
 for f in data/text/*;
 do
-    policy=$(cat "$f" | bash scripts/bash_scripts/getPolicy.sh)
-    cps=$(cat "$f" | bash scripts/bash_scripts/getCPS.sh)
     basic_info=$(scripts/c_scripts/getBasics "$f")
     base_filename="${f##*/}"
-    IFS='-' read -r rank domain <<< $base_filename
-    parsedUrl=${domain%.txt}
-    printf "%s\t%s\t%s\t%s\t%s\t%s\t%s\n" "$rank" "${domain%.txt}" "$basic_info" "$policy" "$cps" >> result.csv
+    fname="${base_filename%.txt}"
+    printf "%s\t%s\t%s\n" "$fname" "$basic_info" >> result.csv
 done
 # Sorts the file in place numerically based on the first column
 sort -k1 -n -o result.csv result.csv
-sort -k1 -n -o data/empty_domains.csv data/empty_domains.csv #sort empty aswell...
 echo "Finished filling result.csv"
